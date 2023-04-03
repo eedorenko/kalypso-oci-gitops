808d19a8cbfa385cd4301fe94cc1e21110873cac
Kalypso Scheduler <kalypso.scheduler@email.com>
2023-04-02 18:05:43 -0700

    Kalypso Scheduler commit

--- vm/hello-world-app-int-test/platform-config.sh
+++ vm/hello-world-app-int-test/platform-config.sh
@@ -1,5 +1,5 @@
-export CLUSTER_NAME=VM
-export ENVIRONMENT=Dev
-export SOME_COMMON_ENVIRONMENT_VARIABLE=false
 export DATABASE_URL=mysql://west:8806/mysql3
 export REGION=West US
+export ENVIRONMENT=Dev
+export SOME_COMMON_ENVIRONMENT_VARIABLE=false
+export CLUSTER_NAME=VM
